787bd873c8c9da7fd2a7f5479b6555d4ffa1aed2
Cris <rmunozp@falabella.cl>
2022-06-17 17:27:53 -0400

    commit formatting concept

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,15 @@ export class AppService {
     return fetch(REPO_COMMITS_URL)
       .then((response) => response.json())
       .then((response) => {
-        return response;
+        const formatResponse = [];
+        response.forEach((res, index) => {
+          const isEven = index % 2 === 1;
+          const commit = `<p style="color:${
+            isEven ? '#FF0000' : '#00FF00'
+          }";>${JSON.stringify(res.commit)}</p>`;
+          formatResponse.push(commit);
+        });
+        return formatResponse.toString();
       });
   }
 }
