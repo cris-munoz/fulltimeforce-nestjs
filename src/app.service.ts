d95482642c8d4f8d26b9d48cecbc678e7c0b6d28
Cris <rmunozp@falabella.cl>
2022-06-17 16:25:17 -0400

    first implementation of getRepoCommits

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+const REPO_COMMITS_URL =
+  'https://api.github.com/repos/cris-munoz/fulltimeforce-nestjs/commits';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getRepoCommits(): any {
+    return fetch(REPO_COMMITS_URL)
+      .then((response) => response.json())
+      .then((response) => {
+        return response;
+      });
   }
 }
